16acfadaa221e6a4e677da24b48fbf10ce2f6096
nspriego <nspriego@gmail.com>
2013-07-09 22:39:59 -0600

    initial commit

---
+++ app/models/task.rb
@@ -0,0 +1,3 @@
+class Task < ActiveRecord::Base
+  attr_accessible :Due_Date, :course, :task
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeworkTracker::Application.config.secret_token = 'f01f4a28442d23baa45d381ce5a98ecc895b49155f5662cbdc6492300567f277026a95e3fdea5b0ee774730ed7b3adccc39d13550b1d943f71444289582a6c9c'
